8870e59642d119b496f73ef8bec13105ac5cf033
Elliot Baik <elbaik412@gmail.com>
2022-03-28 15:42:25 -0500

    Create player prompt

--- rps.js
+++ rps.js
@@ -1,3 +1,4 @@
+// randomly generates one of 'rock', 'paper', or 'scissors' for CPU
 function computerPlay() {
     if (randomNum() == 0) {
         return "rock";
@@ -8,10 +9,27 @@ function computerPlay() {
     }
 }
 
+// randomly generates a number 0-2
 function randomNum() {
     return Math.floor(Math.random() * 3);
 }
 
+// test to check computerPlay is randomly generating each choice
 console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
+
+// get player choice for rps game
+let playerSelection = prompt('Choose one of the following: rock, paper, or scissors');
+
+console.log(playerSelection);
+
+// get computer choice for rps game
+let computerSelection = computerPlay();
+
+console.log(computerSelection);
+
+// // function to play one round of rps
+// function playRound(playerSelection, computerSelection) {
+//     if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+//         return "Player wins!";
+//     }
+// }
