2037df39294e1fc6d78619c40fe99a7885fa5749
Elliot Baik <elbaik412@gmail.com>
2022-03-28 16:47:47 -0500

    Complete playRound function

--- rps.js
+++ rps.js
@@ -18,7 +18,7 @@ function randomNum() {
 console.log(computerPlay());
 
 // get player choice for rps game
-let playerSelection = prompt('Choose one of the following: rock, paper, or scissors');
+let playerSelection = prompt('Choose one of the following: rock, paper, or scissors').toLowerCase();
 
 console.log(playerSelection);
 
@@ -27,9 +27,27 @@ let computerSelection = computerPlay();
 
 console.log(computerSelection);
 
-// // function to play one round of rps
-// function playRound(playerSelection, computerSelection) {
-//     if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
-//         return "Player wins!";
-//     }
-// }
+let win = "Player wins!";
+let lose = "Player loses!";
+let draw = "It's a tie! Go again!";
+
+// function to play one round of rps
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return win;
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return lose;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return win;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return lose;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return win;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return lose;
+    } else {
+        return draw;
+    }
+}
+console.log(playRound(playerSelection, computerSelection));
+
