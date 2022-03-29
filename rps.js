a4d720ed5b6787e04e8a9a55c8619edc042e2c08
Elliot Baik <elbaik412@gmail.com>
2022-03-29 18:42:50 -0500

    Adjust playRound function

--- rps.js
+++ rps.js
@@ -1,4 +1,4 @@
-// randomly generates one of 'rock', 'paper', or 'scissors' for CPU
+// randomly generates one of 'rock', 'paper', or 'scissors' for CPU based on random number result
 function computerPlay() {
     if (randomNum() == 0) {
         return "rock";
@@ -9,7 +9,7 @@ function computerPlay() {
     }
 }
 
-// randomly generates a number 0-2
+// randomly generates a number 0-2, which determines what the computer chooses
 function randomNum() {
     return Math.floor(Math.random() * 3);
 }
@@ -17,61 +17,55 @@ function randomNum() {
 // test to check computerPlay is randomly generating each choice
 console.log(computerPlay());
 
-// get player choice for rps game
-let playerSelection = prompt('Choose one of the following: rock, paper, or scissors').toLowerCase();
+// player choice is unknown until playRound called
+let playerSelection;
 
-console.log(playerSelection);
+// function to play one round of rps when playerSelection and computerSelection are passed into function
+function playRound() {
+    // preset messages to display on win/lose/draw results
+    let win = "You win the round!";
+    let lose = "You lose the round!";
+    let draw = "It is a tie! Go again!";
 
-// get computer choice for rps game
-let computerSelection = computerPlay();
+    // prompt player to make choice
+    let playerSelection = prompt("Choose one of the following: rock, paper, or scissors.").toLowerCase();
+    console.log(playerSelection);
 
-console.log(computerSelection);
+    // get computer choice for rps game and assign to computerSelection
+    let computerSelection = computerPlay();
+    console.log(computerSelection);
 
-// messages to print on player win/lose
-let win = "Player wins!";
-let lose = "Player loses!";
-let draw = "It's a tie! Go again!";
-
-// initial score count for both player and computer
-let playerTally = 0;
-let computerTally = 0;
-
-// function to play one round of rps, player gets +1 point on wins, and computer gets +1 on player losses. +0 for draws.
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerTally++;
+    if (playerSelection == "rock" && computerSelection == "scissors") {
         return win;
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        computerTally++;
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
         return lose;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerTally++;
-        return win;
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerTally++;
-        return lose;
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        playerTally++;
-        return win;
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerTally++;
-        return lose;
-    } else {
+    } else if (playerSelection == "rock" && computerSelection == "rock") {
         return draw;
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return win;
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return lose;
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        return draw;
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return win;
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return lose;
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        return draw;
+    } else {
+        return "Not a valid choice";
     }
 }
-console.log(playRound(playerSelection, computerSelection));
 
+// test playRound function
+console.log(playRound());
 
-
+// function that repeats playRound 5 times
 function game() {
-    for (let i = 0; i < 5; i++) {}
-}
-
-// function called for when either player or computer reaches 5 points
-function gameOver() {
-    if (playerTally == 5 || computerTally == 5) {
-        return "Game Over!";
+// loops playRound 5 times
+    for (i = 0; i < 5; i++) {
+        playRound();
     }
-}
 
+}
